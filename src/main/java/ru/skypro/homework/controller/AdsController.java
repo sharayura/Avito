d65a3d1ac1a5d37cb95afbec5ef7755391e48994
sharayura <sharapyura@gmail.com>
2023-05-25 16:20:07 +0300

    Controllers

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,21 +1,16 @@
 package ru.skypro.homework.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.skypro.homework.dto.AdsDto;
-import ru.skypro.homework.dto.CreateAdsDto;
-import ru.skypro.homework.dto.RegisterReqDto;
-import ru.skypro.homework.dto.Role;
-
-import static ru.skypro.homework.dto.Role.USER;
+import ru.skypro.homework.dto.*;
 
 @RestController
 @RequestMapping("ads")
@@ -52,7 +47,7 @@ public class AdsController {
                             description = "Created",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = AdsDto.class)
+                                    schema = @Schema(implementation = AdsDto.class)    //TODO
                             )
                     ),
                     @ApiResponse(
@@ -62,6 +57,308 @@ public class AdsController {
             })
     @PostMapping()
     public ResponseEntity<?> addAd(@RequestBody CreateAdsDto createAds) {
-            return ResponseEntity.ok().build();
+        return ResponseEntity.ok().build();
     }
+
+    @Operation(summary = "Получить комментарии объявления",
+            tags = "Комментарии",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = CommentDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    )
+            })
+    @GetMapping("{id}/comments")
+    public ResponseEntity<?> getComments(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Добавить комментарий к объявлению",
+            tags = "Комментарии",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = CreateCommentDto.class)
+                    )
+            ),
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = CommentDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    )
+            })
+    @PostMapping("{id}/comments")
+    public ResponseEntity<?> addComment(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Получить информацию об объявлении",
+            tags = "Объявления",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema()      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = FullAdsDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    )
+            })
+    @GetMapping("{id}")
+    public ResponseEntity<?> getAds(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Удалить объявление",
+            tags = "Объявления",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema()      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "No Content"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    )
+            })
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> removeAd(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Обновить информацию об объявлении",
+            tags = "Объявления",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = CreateAdsDto.class)
+                    )
+            ),
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = AdsDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    )
+            })
+    @PatchMapping("{id}")
+    public ResponseEntity<?> updateAds(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Удалить комментарий",
+            tags = "Комментарии",
+            parameters = {
+                    @Parameter(
+                            name = "adId",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema()      //TODO
+                    ),
+                    @Parameter(
+                            name = "commentId",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema()      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    )
+            })
+    @DeleteMapping("{adId}/comments/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable Long adId, @PathVariable Long commentId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Обновить комментарий",
+            tags = "Комментарии",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = CommentDto.class)
+                    )
+            ),
+            parameters = {
+                    @Parameter(
+                            name = "adId",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema()      //TODO
+                    ),
+                    @Parameter(
+                            name = "commentId",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema()      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = CommentDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    )
+            })
+    @PatchMapping("{adId}/comments/{commentId}")
+    public ResponseEntity<?> updateComment(@PathVariable Long adId, @PathVariable Long commentId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Получить объявления авторизованного пользователя",
+            tags = "Объявления",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = FullAdsDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    )
+            })
+    @GetMapping("me")
+    public ResponseEntity<?> getAdsMe() {
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Обновить картинку объявления",
+            tags = "Объявления",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
+                            schema = @Schema()     //TODO
+                    )
+            ),
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            schema = @Schema      //TODO
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE,
+                                    schema = @Schema()      //TODO
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    )
+            })
+    @PatchMapping("{id}/image")
+    public ResponseEntity<?> updateImage(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
