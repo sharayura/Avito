4962fa82c0df7520ccdca72fbdc5ff492f3e90ca
sergeydomanov <nomad-labs@yandex.ru>
2023-05-31 21:51:50 +0300

    Добавлены entity

---
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -0,0 +1,22 @@
+package ru.skypro.homework.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private int author;
+    private String authorImage;
+    private String authorFirstName;
+    private LocalDateTime createdAt;
+    private int pk;
+    private String text;
+}
